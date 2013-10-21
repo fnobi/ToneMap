99daec60ebb7df6bf7c1c2914b8332af12a72297
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-10-22 00:51:16 +0900

    スピードに応じて、音の高さを変更。

--- js/ToneMap-demo.js
+++ js/ToneMap-demo.js
@@ -314,23 +314,53 @@ function init () {
         var interval = 50;
         var playbackRate = 1.0;
 
+        var prevPos;
+
         var loop;
-        function startSound () {
+        function startSound (e) {
             loop = setInterval(function () {
                 toneMap.play('tap', {
                     playbackRate: playbackRate
                 });
             }, interval);
+
+            prevPos = [
+                e.touches ? e.touches[0].pageX : e.pageX,
+                e.touches ? e.touches[0].pageY : e.pageY
+            ];
         }
 
         function stopSound () {
             clearInterval(loop);
+            loop = null;
+            prevPos = [];
+        }
+
+        function processPointer (e) {
+            if (!loop) {
+                return;
+            }
+
+            var pos = [
+                e.touches ? e.touches[0].pageX : e.pageX,
+                e.touches ? e.touches[0].pageY : e.pageY
+            ];
+
+            var dx = pos[0] - prevPos[0];
+            var dy = pos[1] - prevPos[1];
+            var d = Math.sqrt(dx * dx + dy * dy);
+
+            playbackRate = Math.max(d / 5, 0.1);
+
+            prevPos = pos;
         }
 
         el.addEventListener('mousedown', startSound, false);
+        el.addEventListener('mousemove', processPointer, false);
         el.addEventListener('mouseup', stopSound, false);
 
         el.addEventListener('touchstart', startSound, false);
+        el.addEventListener('touchmove', processPointer, false);
         el.addEventListener('touchend', stopSound, false);
     };
 }
